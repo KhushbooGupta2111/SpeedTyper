16db6c1f3543a440cc95d64553cdd31975961cd0
Khushboo <guptakhushboo81537@gmail.com>
2022-02-07 01:55:36 +0530

    Code refactored

--- level.js
+++ level.js
@@ -1,20 +1,20 @@
-var countDownDate = new Date("December 31, 2024 15:37:25").getTime();
-var x = setInterval(function() {
-  now = new Date().getTime();
-  distance = countDownDate - now;
-  seconds = Math.floor((distance % (1000 * 60)) / 1000);
-  document.getElementById("demo").innerHTML =  seconds + "s ";
-},1000);
+function setTimer() {
+ setInterval(function() {
+ 
+},10000);
+}
 
 
-const words = ['hello', 'world','apple','nice','school','git'];
+const words = ['hello', 'world','apple','nice','school','doughnut','daring','fearless'];
+
 
 function isCorrect() {
-     input1 = document.body.querySelector("word-input").value;
-    if(input1 == words[0])
-    {
-      document.body.querySelector("text").innerHTML = "Hello"
-    }
+     //input1 = document.body.querySelector("word-input").value;
+     document.querySelector("text").innerHTML = "Hello";
+    // if(input1 == words[0])
+    // {
+    //   document.body.querySelector("text").innerHTML = "Hello";
+    // }
     
 }
 
