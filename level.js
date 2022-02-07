1508a71050b15bbf3838239e9aad4e53ee6dafd0
Khushboo <guptakhushboo81537@gmail.com>
2022-02-07 22:41:34 +0530

    Code refactored

--- level.js
+++ level.js
@@ -1,20 +1,59 @@
+window.addEventListener('load',startGame);
+const words = ["hello", "world","apple","nice","school","doughnut","daring","fearless"];
+var score = 0;
+var count = 10;
+let intervalId;
+
 function setTimer() {
- setInterval(function() {
- 
-},10000);
+ intervalId = setInterval(countDown,1000);
+}
+
+function startGame()
+{
+    setTimer();
+    Word();
+    const word1 = document.querySelector("#word-input").addEventListener('change',isCorrect);
+}
+
+const time = document.querySelector(".time-score-number");
+
+function countDown(){
+    if(count === 0)
+{
+    alert("Game is over");
+    if(intervalId)
+    clearInterval(intervalId);
+}
+
+time.innerText = count;
+count--;
+
+
 }
 
 
-const words = ['hello', 'world','apple','nice','school','doughnut','daring','fearless'];
+function Word(){
+    var wordNumber = Math.floor(Math.random() * words.length)
+    var typeWord = words[wordNumber];
+    document.getElementById("word").innerText = typeWord;
+    
+
+
+}
 
 
 function isCorrect() {
-     //input1 = document.body.querySelector("word-input").value;
-     document.querySelector("text").innerHTML = "Hello";
-    // if(input1 == words[0])
-    // {
-    //   document.body.querySelector("text").innerHTML = "Hello";
-    // }
     
+    const word = document.querySelector("#word").innerText;
+    const input = document.querySelector("#word-input").value;
+    if(word === input){
+         Word();
+        score += 10;
+        count += 10;
+        document.querySelector(".time-score").innerText = count;
+    }
+     //document.querySelector("text").innerHTML = "Hello";
 }
+
+
 
